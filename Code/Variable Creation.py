950dc48ab3bc8b9df84187fa46fe3531142f4a4d
Nuka <157550461+NukaNekoTTV@users.noreply.github.com>
2025-03-05 12:12:36 -0500

    Started

---
+++ Code/Variable Creation.py
@@ -0,0 +1,34 @@
+#There is multiple datatypes to use while doing Variable Creation, These are just a few
+
+#String Datatype
+Response = "Hello!"
+Response = "3" #This is the word 3, counted as a string, not the number
+
+#Boolean/Bool Datatype
+
+#True Datatype
+Answered = True
+
+#False Datatype
+Answered = False
+
+#Integer Datatype
+
+#Keep in mind: zero, positive, or negative whole numbers with no fractional part and infinite precision
+
+#Positive
+Value = 3
+
+#Negative
+Value = -3
+
+#Zero
+Value = 0
+
+#Float Datatype (Integer version but with Decimals)
+
+#Positive
+Value = 9
+
+#Decimal
+Value = 9.3
