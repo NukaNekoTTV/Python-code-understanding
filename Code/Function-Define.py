c4f3a8a38badbe1b43303ff3d09c363309ef77c6
Nuka <157550461+NukaNekoTTV@users.noreply.github.com>
2025-03-05 12:19:51 -0500

    Create Function-Define.py

---
+++ Code/Function-Define.py
@@ -0,0 +1,4 @@
+def commandprompt(): #We used "Def" as the function from Java, and commandprompt is our function name
+  print("whatever") #This will print after it passes the function, Now unlike Lua we dont use a end function instead we used:
+  return #This would return the prompt or:
+  break #This would break the code, Stopping it from going farther than this, used for if-then statement to stop it
